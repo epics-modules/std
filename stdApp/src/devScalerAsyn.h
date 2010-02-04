78d4773346de9ed8d884b4d69238502d5308e8ad
MarkRivers <rivers@cars.uchicago.edu>
2010-02-04 00:31:24 +0000

    Added SCALER_READ_SINGLE command

--- stdApp/src/devScalerAsyn.h
+++ stdApp/src/devScalerAsyn.h
@@ -11,11 +11,12 @@
  * Drivers must return a value in pasynUser->reason that is unique for that command.
  */
 
-#define SCALER_RESET_COMMAND_STRING    "SCALER_RESET"    /* int32, write */
-#define SCALER_CHANNELS_COMMAND_STRING "SCALER_CHANNELS" /* int32, read */
-#define SCALER_READ_COMMAND_STRING     "SCALER_READ"     /* int32Array, read */
-#define SCALER_PRESET_COMMAND_STRING   "SCALER_PRESET"   /* int32, write */
-#define SCALER_ARM_COMMAND_STRING      "SCALER_ARM"      /* int32, write */
-#define SCALER_DONE_COMMAND_STRING     "SCALER_DONE"     /* int32, read */
+#define SCALER_RESET_COMMAND_STRING       "SCALER_RESET"        /* int32, write */
+#define SCALER_CHANNELS_COMMAND_STRING    "SCALER_CHANNELS"     /* int32, read */
+#define SCALER_READ_COMMAND_STRING        "SCALER_READ"         /* int32Array, read */
+#define SCALER_READ_SINGLE_COMMAND_STRING "SCALER_READ_SINGLE"  /* int32, read */
+#define SCALER_PRESET_COMMAND_STRING      "SCALER_PRESET"       /* int32, write */
+#define SCALER_ARM_COMMAND_STRING         "SCALER_ARM"          /* int32, write */
+#define SCALER_DONE_COMMAND_STRING        "SCALER_DONE"         /* int32, read */
 
-#define MAX_SCALER_COMMANDS 6
+#define MAX_SCALER_COMMANDS 7
